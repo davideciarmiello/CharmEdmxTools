8e37d2283d4310e36c7e3fc727d360734ca6958d
davideciarmiello <davide.ciarmiello@gmail.com>
2018-05-16 13:58:02 +0200

    rimosso proj inutile

--- src/CharmEdmxTools.2017/Update.cmd
+++
@@ -1,5 +0,0 @@
-"C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\IDE\VSIXInstaller.exe" /uninstall:1601c91e-ce52-4571-b547-b26295df8ec8
-
-"C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\IDE\VSIXInstaller.exe" "%~dp0bin\Debug\CharmEdmxTools.vsix"
-
-pause
